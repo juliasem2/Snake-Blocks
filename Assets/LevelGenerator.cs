12a81d3b9f57f6d13d23f8a6cd59140074373dd7
juliasem2 <63554867+juliasem2@users.noreply.github.com>
2022-09-13 20:01:15 +0300

    Change in LevelGenerator

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject[] RowsPrefabs;
+    //public GameObject Coin;
+    public float DistanceBetweenRows;
+    public int NumberRows;
+    public GameObject HeadSnake;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Awake()
+    {
+        for (int i = 0; i < NumberRows; i++)
+        {
+            int prefabIndex = Random.Range(0, RowsPrefabs.Length);
+            Vector3 position = new Vector3(0, 0, DistanceBetweenRows * i);
+            Quaternion rotation = Quaternion.Euler(0, 0, 0);
+            GameObject body = Instantiate(RowsPrefabs[prefabIndex], position, rotation, transform);
+
+            //var collisionComponent = body.GetComponent<Collision>();
+            //collisionComponent.HeadSnake = HeadSnake.transform;
+
+            //body.GetComponent<Collision>().HeadSnake = HeadSnake.transform;
+            var arrayCollision = body.GetComponentsInChildren<Collision>();
+
+            foreach (Collision collision in arrayCollision)
+            {
+                collision.HeadSnake = HeadSnake.transform;
+            }           
+        }
+    }
+}
